452b859f9fcef8a65022debc2c986b6878e4a388
Nicolas <alternative.rvb@gmail.com>
2023-01-15 13:41:12 +0100

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+import {getRandomColorHue, HSLToHex} from "./randomColor.js";
+
+// SECTION PLUGINS SHODOWN MARDOWN
+const converter = new showdown.Converter();
+converter.setOption("tasklists", "true");
+converter.setOption("tables", "true");
+// !SECTION
+
+// SECTION TODO APP
 const modalForm = document.querySelector("#modalForm");
 const btnToOPen = document.querySelector("#btn-create");
 const btnToClose = document.querySelectorAll(".btn-close");
@@ -7,140 +16,71 @@ const info = document.querySelector("#info");
 const tasks = document.querySelector("#tasks");
 const initInfo = "Pour créer une tâche appuyer sur +";
 
-// SECTION PLUGINS SHODOWN MARDOWN
-const converter = new showdown.Converter();
-converter.setOption("tasklists", "true");
-converter.setOption("tables", "true");
-// !SECTION
-
-// SECTION GESTION DE LA COULEUR
-
-const getRandomColorHue = () => {
-  return Math.floor(Math.random() * 360 + 1);
-};
-
-console.log("getRandomColorHue ===>", getRandomColorHue());
-
-function HSLToHex(h, s, l) {
-  s /= 100;
-  l /= 100;
-
-  let c = (1 - Math.abs(2 * l - 1)) * s,
-    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
-    m = l - c / 2,
-    r = 0,
-    g = 0,
-    b = 0;
-
-  if (0 <= h && h < 60) {
-    r = c;
-    g = x;
-    b = 0;
-  } else if (60 <= h && h < 120) {
-    r = x;
-    g = c;
-    b = 0;
-  } else if (120 <= h && h < 180) {
-    r = 0;
-    g = c;
-    b = x;
-  } else if (180 <= h && h < 240) {
-    r = 0;
-    g = x;
-    b = c;
-  } else if (240 <= h && h < 300) {
-    r = x;
-    g = 0;
-    b = c;
-  } else if (300 <= h && h < 360) {
-    r = c;
-    g = 0;
-    b = x;
-  }
-  // Having obtained RGB, convert channels to hex
-  r = Math.round((r + m) * 255).toString(16);
-  g = Math.round((g + m) * 255).toString(16);
-  b = Math.round((b + m) * 255).toString(16);
-
-  // Prepend 0s, if necessary
-  if (r.length == 1) r = "0" + r;
-  if (g.length == 1) g = "0" + g;
-  if (b.length == 1) b = "0" + b;
-
-  return "#" + r + g + b;
-}
-
-console.log("HSLToHex ===>", HSLToHex(getRandomColorHue(), 50, 50));
-
-// !SECTION
-
-// SECTION TODO APP
-
-const noRefresh = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-};
-
 btnToOPen.addEventListener("click", (e) => {
-  noRefresh(e);
-  modalForm.style.display = "block";
-  // resetForm();
+    noRefresh(e);
+    modalForm.style.display = "block";
+    // resetForm();
 });
 
 btnToClose.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    noRefresh(e);
-    modalForm.style.display = "none";
-  });
+    item.addEventListener("click", (e) => {
+        noRefresh(e);
+        modalForm.style.display = "none";
+    });
 });
 
-const formValidation = () => {
-  if (inputTitle.value === "") {
-    console.log("failure");
-    info.innerHTML = "Task cannot be blank";
-    info.classList.add("color-danger");
-  } else {
-    console.log("success");
-    info.innerHTML = "";
-    acceptData();
-    modalForm.style.display = "none";
-    // FIXME
-    // btn-add.setAttribute("data-bs-dismiss", "modal");
-    // btn-add.click();
-    // (() => {
-    //   btn-add.setAttribute("data-bs-dismiss", "");
-    // })();
-  }
-};
+function noRefresh(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+function formValidation() {
+    if (inputTitle.value === "") {
+        console.log("failure");
+        info.innerHTML = "Task cannot be blank";
+        info.classList.add("color-danger");
+    } else {
+        console.log("success");
+        info.innerHTML = "";
+        acceptData();
+        modalForm.style.display = "none";
+        // FIXME
+        // btn-add.setAttribute("data-bs-dismiss", "modal");
+        // btn-add.click();
+        // (() => {
+        //   btn-add.setAttribute("data-bs-dismiss", "");
+        // })();
+    }
+}
 
 modalForm.addEventListener("submit", (e) => {
-  noRefresh(e);
-  formValidation();
+    noRefresh(e);
+    formValidation();
 });
 
 // ANCHOR ACCEPT DATA
 let data = [];
-const acceptData = () => {
-  // data.push({object})
-  data.unshift({
-    title: taskForm.inputTitle.value,
-    date: taskForm.inputDate.value,
-    description: taskForm.textAreaMsg.value,
-    color: taskForm.inputColor.value,
-  });
-  localStorage.setItem("data", JSON.stringify(data));
-  createTasks();
-};
+function acceptData() {
+    // data.push({object})
+    data.unshift({
+        title: taskForm.inputTitle.value,
+        date: taskForm.inputDate.value,
+        description: taskForm.textAreaMsg.value,
+        color: taskForm.inputColor.value,
+    });
+    localStorage.setItem("data", JSON.stringify(data));
+    createTasks();
+}
 
 // ANCHOR CREATE TASK
-const createTasks = () => {
-  tasks.innerHTML = "";
-  btnToAdd.innerHTML = "Add";
-  data.map((x, y) => {
-    return (tasks.innerHTML += `
+function createTasks() {
+    tasks.innerHTML = "";
+    btnToAdd.innerHTML = "Add";
+    data.map((x, y) => {
+        return (tasks.innerHTML += `
       <article id=${y} class="tasks__art d-flex fd-column" style="border-color:${
-      x.color
-    }">
+            x.color
+        }">
       <h3 class="tasks__title">${x.title}</h3>
       <p class="tasks__meta">${x.date}</p>
       <pre class="tasks__description">${converter.makeHtml(x.description)}</pre>
@@ -154,45 +94,45 @@ const createTasks = () => {
       </form>
       </article>
       `);
-  });
-  resetForm();
+    });
+    resetForm();
 
-  console.log({data});
-};
+    console.log({data});
+}
 
 // ANCHOR UPDATE DATA
-const editTask = (e) => {
-  modalForm.style.display = "block";
-  btnToAdd.innerHTML = "Update";
-  let selectedTask = e.parentElement.parentElement;
-  taskForm.inputTitle.value = data[selectedTask.id].title;
-  taskForm.inputDate.value = data[selectedTask.id].date;
-  taskForm.textAreaMsg.value = data[selectedTask.id].description;
-  taskForm.inputColor.value = data[selectedTask.id].color;
-  deleteTask(e);
-  console.log({data});
-};
+function editTask(e) {
+    modalForm.style.display = "block";
+    btnToAdd.innerHTML = "Update";
+    let selectedTask = e.parentElement.parentElement;
+    taskForm.inputTitle.value = data[selectedTask.id].title;
+    taskForm.inputDate.value = data[selectedTask.id].date;
+    taskForm.textAreaMsg.value = data[selectedTask.id].description;
+    taskForm.inputColor.value = data[selectedTask.id].color;
+    deleteTask(e);
+    console.log({data});
+}
 
 // ANCHOR DELETE TASK
-const deleteTask = (e) => {
-  e.parentElement.parentElement.remove();
-  data.splice(e.parentElement.parentElement.id, 1);
-  localStorage.setItem("data", JSON.stringify(data));
-  console.log({data});
-};
+function deleteTask(e) {
+    e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.id, 1);
+    localStorage.setItem("data", JSON.stringify(data));
+    console.log({data});
+}
 
 // ANCHOR RESET FORM
-const resetForm = () => {
-  taskForm.inputTitle.value = "";
-  taskForm.inputDate.value = "";
-  taskForm.textAreaMsg.value = "";
-  taskForm.inputColor.value = HSLToHex(getRandomColorHue(), 50, 50);
-};
+function resetForm() {
+    taskForm.inputTitle.value = "";
+    taskForm.inputDate.value = "";
+    taskForm.textAreaMsg.value = "";
+    taskForm.inputColor.value = HSLToHex(getRandomColorHue(), 50, 50);
+}
 
 (() => {
-  data = JSON.parse(localStorage.getItem("data")) || [];
-  console.log({data});
-  createTasks();
+    data = JSON.parse(localStorage.getItem("data")) || [];
+    console.log({data});
+    createTasks();
 })();
 
 // !SECTION
