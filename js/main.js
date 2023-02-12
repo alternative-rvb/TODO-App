128da97b4fcf8b02ca614c4d9b46b3ff6350eb5b
Nicolas <alternative.rvb@gmail.com>
2023-02-12 01:42:24 +0100

    Ajout fonctionnalité checkbox

--- js/main.js
+++ js/main.js
@@ -4,11 +4,10 @@ import {sanitizeInput} from "./tools.js";
 const modalForm = document.querySelector("#modal-form");
 const taskForm = document.querySelector("#task-form");
 const btnCreate = document.querySelector("#btn-create");
-const tasks = document.querySelector("#tasks-container");
-const inputContainer = document.querySelector("#inputs-container");
-let countInputFields = 0;
-console.log("countInputFields => ", countInputFields);
-const btnAdd = document.querySelector("#btn-add");
+const listsContainer = document.querySelector("#lists-container");
+const inputsContainer = document.querySelector("#inputs-container");
+let countInputs = 0;
+console.log("countInputs => ", countInputs);
 const info = document.querySelector("#info");
 
 // SECTION TODO APP
@@ -20,7 +19,7 @@ function noRefresh(e) {
 
 btnCreate.addEventListener("click", (e) => {
     noRefresh(e);
-    createTasks();
+    createList();
     modalForm.style.display = "block";
     taskForm.inputTitle.focus();
     taskForm.inputColor.value = getRandomColor(50, 50);
@@ -39,24 +38,24 @@ modalForm.onkeypress = (e) => {
 
 modalForm.querySelector(".add-single").addEventListener("click", (e) => {
     // noRefresh(e);
-    addInput(inputContainer);
+    addInput(inputsContainer);
 });
 // Lorsqu'on appuie sur entrée on ajoute un autre input
-inputContainer.addEventListener("keyup", (e) => {
+inputsContainer.addEventListener("keyup", (e) => {
     if (e.key === "Enter") {
         // noRefresh(e);
-        addInput(inputContainer);
+        addInput(inputsContainer);
     }
 });
 
-modalForm.querySelector(".btn-add").addEventListener("click", (e) => {
+modalForm.querySelector(".btn-validation").addEventListener("click", (e) => {
     noRefresh(e);
     formValidation();
 });
 
 modalForm.querySelector(".btn-reset").addEventListener("click", (e) => {
     // noRefresh(e);
-    createTasks();
+    createList();
 });
 
 modalForm.querySelector(".btn-close").addEventListener("click", (e) => {
@@ -65,10 +64,10 @@ modalForm.querySelector(".btn-close").addEventListener("click", (e) => {
     //  let confirmation=  confirm("Voulez-vous vraiment quitter ? Vous allez perdre vos données");
     // if (confirmation) {
     //     modalForm.style.display = "none";
-    //     createTasks();
+    //     createList();
     // }
     modalForm.style.display = "none";
-    createTasks();
+    createList();
     info.innerHTML = "";
 });
 
@@ -86,10 +85,10 @@ function formValidation() {
         acceptData();
         modalForm.style.display = "none";
         // FIXME
-        // btn-add.setAttribute("data-bs-dismiss", "modal");
-        // btn-add.click();
+        // btn-validation.setAttribute("data-bs-dismiss", "modal");
+        // btn-validation.click();
         // (() => {
-        //   btn-add.setAttribute("data-bs-dismiss", "");
+        //   btn-validation.setAttribute("data-bs-dismiss", "");
         // })();
     }
 }
@@ -109,7 +108,14 @@ function acceptData() {
                     taskForm.elements[i].value
                 );
             } else if (taskForm.elements[i].value) {
-                obj["tasks"].push(sanitizeInput(taskForm.elements[i].value));
+                // obj["tasks"].push(sanitizeInput(taskForm.elements[i].value));
+                obj.tasks.push({
+                    isChecked:
+                        taskForm.elements[i].dataset.checked === "true"
+                            ? "true"
+                            : "false",
+                    task: sanitizeInput(taskForm.elements[i].value),
+                });
             }
         }
     }
@@ -118,15 +124,15 @@ function acceptData() {
     });
 
     localStorage.setItem("data", JSON.stringify(data));
-    createTasks();
+    createList();
 }
 
-// ANCHOR CREATE TASK | AFFICHAGE | UI
-function createTasks() {
-    tasks.innerHTML = "";
-    btnAdd.innerHTML = "Ajouter";
+// ANCHOR CREATE LIST | AFFICHAGE | UI
+function createList() {
+    listsContainer.innerHTML = "";
+    document.querySelector("#btn-validation").innerHTML = "Ajouter";
     data.map((x) => {
-        return (tasks.innerHTML += `
+        return (listsContainer.innerHTML += `
       <article id=${
           x.id
       } class="tasks__art d-flex fd-column" style="border-color:${
@@ -140,10 +146,12 @@ function createTasks() {
       }
       <ul>
         ${x.tasks
-            .map((item) => {
-                if (item) {
-                    return `<li class="tasks__li">${item}</li>`;
-                }
+            .map((y) => {
+                return `<li class="tasks__li ${
+                    y.isChecked === "true" ? "checked" : ""
+                }" data-checked="${
+                    y.isChecked === "true" ? "true" : "false"
+                }">${y.task}</li>`;
             })
             .join("")}
       </ul>
@@ -160,55 +168,76 @@ function createTasks() {
     });
     // ANCHOR UI
 
-    const edit = tasks.querySelectorAll(".edit-task");
+    const edit = listsContainer.querySelectorAll(".edit-task");
     edit.forEach((item) => {
         item.addEventListener("click", (e) => {
             noRefresh(e);
-            editTask(e);
+            updateList(e);
         });
     });
 
-    const deleteBtn = tasks.querySelectorAll(".delete-task");
+    const deleteBtn = listsContainer.querySelectorAll(".delete-task");
     deleteBtn.forEach((item) => {
         item.addEventListener("click", (e) => {
             noRefresh(e);
-            deleteTask(e);
-            createTasks();
+            deleteList(e);
+            createList();
+        });
+    });
+
+    const tasksLi = listsContainer.querySelectorAll(".tasks__li");
+    tasksLi.forEach((item) => {
+        item.addEventListener("click", (e) => {
+            noRefresh(e);
+            checkTask(e);
         });
     });
 
     taskForm.reset();
-    inputContainer.innerHTML = "";
-    countInputFields = 1;
-    addInput(inputContainer);
+    inputsContainer.innerHTML = "";
+    countInputs = 1;
+    addInput(inputsContainer);
     console.log("data created", {data});
 }
 
-// ANCHOR UPDATE DATA
-function editTask(e) {
+// ANCHOR UPDATE LIST
+function updateList(e) {
     modalForm.style.display = "block";
-    btnAdd.innerHTML = "Mettre à jour";
+    document.querySelector("#btn-validation").innerHTML = "Mettre à jour";
     const selectedTask = e.currentTarget.closest(".tasks__art");
     const allTasks = selectedTask.querySelectorAll(".tasks__li");
     const index = data.findIndex((item) => item.id == selectedTask.id);
     console.log("index => ", index);
     if (data[index].tasks.length > 0) {
         for (let i = 1; i < data[index].tasks.length; i++) {
-            addInput(inputContainer);
+            addInput(inputsContainer);
         }
     }
     taskForm.elements.inputTitle.value = data[index].title;
     taskForm.elements.inputDate.value = data[index].date;
     taskForm.elements.inputColor.value = data[index].color;
     allTasks.forEach((item, index) => {
-        taskForm.elements[`input-task${index + 1}`].value = item.innerText;
+        taskForm.elements[`inputTask${index + 1}`].value = item.innerText;
+        taskForm.elements[`inputTask${index + 1}`].dataset.checked =
+            item.dataset.checked;
     });
 
-    deleteTask(e);
+    deleteList(e);
+}
+// UPDATE TASK
+function autoUpdateTasks(e) {
+    const selectedTask = e.currentTarget.closest(".tasks__art");
+    const allTasks = selectedTask.querySelectorAll(".tasks__li");
+    const indexOfList = data.findIndex((item) => item.id == selectedTask.id);
+    data[indexOfList].tasks.forEach((item, indexOfTask) => {
+        item.task = allTasks[indexOfTask].innerText;
+        item.isChecked = allTasks[indexOfTask].dataset.checked;
+    });
+    localStorage.setItem("data", JSON.stringify(data));
 }
 
-// ANCHOR DELETE TASK
-function deleteTask(e) {
+// ANCHOR DELETE LIST
+function deleteList(e) {
     const selectedTask = e.currentTarget.closest(".tasks__art");
     const index = data.findIndex((item) => item.id == selectedTask.id);
 
@@ -221,34 +250,42 @@ function addInput(location) {
     const newInput = document.createElement("div");
     newInput.classList.add("my-1", "d-flex");
     newInput.innerHTML = `
-    <input type="text" id="input-task${countInputFields}" class="single-task  flex-1 rounded-left" placeholder="Tâche ${countInputFields}..." name="task1">
+    <input type="text" id="inputTask${countInputs}" class="single-task  flex-1 rounded-left" placeholder="Tâche ${countInputs}..." name="task1">
     <button type="button" class="remove-single btn-default rounded-right">-</button>
     `;
     location.appendChild(newInput);
     newInput.querySelector(".remove-single").addEventListener("click", (e) => {
         console.log("e => ", e);
         // noRefresh(e);
-        if (countInputFields > 2) {
+        if (countInputs > 2) {
             e.target.parentElement.remove();
-            countInputFields--;
-            console.log("countInputFields => ", countInputFields);
+            countInputs--;
+            console.log("countInputs => ", countInputs);
         } else {
-            // Sélectionner le premier enfant de inputContainer
+            // Sélectionner le premier enfant de inputsContainer
             e.target.parentElement.children[0].value = "";
         }
     });
     // Ajouter focus sur le dernier input
     newInput.querySelector(".single-task").focus();
-    countInputFields++;
-    console.log("countInputFields => ", countInputFields);
+    countInputs++;
+    console.log("countInputs => ", countInputs);
 
     return newInput;
+}
+
+// ANCHOR CHECK TASK
+function checkTask(e) {
+    e.currentTarget.dataset.checked =
+        e.currentTarget.dataset.checked === "false" ? "true" : "false";
+    e.currentTarget.classList.toggle("checked");
+    autoUpdateTasks(e);
 }
 
 (() => {
     data = JSON.parse(localStorage.getItem("data")) || [];
     console.log("data onLoad", {data});
-    createTasks();
+    createList();
 })();
 
 // !SECTION
@@ -260,37 +297,37 @@ function addInput(location) {
 // ondragover
 // ondrop
 
-const allArticles = document.querySelectorAll(".tasks__art");
+// const allArticles = document.querySelectorAll(".tasks__art");
 
-allArticles.forEach((article) => {
-    article.ondragstart = (e) => {
-        console.log("e.target START => ", e.target);
-        e.target.closest("article").classList.add("drag-start");
-        // e.dataTransfer.setData("text/plain", e.target.closest("article").innerHTML);
-        // e.currentTarget.style.transform = "scale(.5)";
-    };
-    article.ondragover = (e) => {
-        e.preventDefault();
-    };
+// allArticles.forEach((article) => {
+//     article.ondragstart = (e) => {
+//         console.log("e.target START => ", e.target);
+//         e.target.closest("article").classList.add("drag-start");
+//         // e.dataTransfer.setData("text/plain", e.target.closest("article").innerHTML);
+//         // e.currentTarget.style.transform = "scale(.5)";
+//     };
+//     article.ondragover = (e) => {
+//         e.preventDefault();
+//     };
 
-    article.ondragenter = (e) => {
-        console.log("e.target ENTER => ", e.target.closest("article"));
-        e.target.closest("article").classList.remove("drag-start");
-        // e.target.closest("article").classList.add("drag-enter");
-    };
+//     article.ondragenter = (e) => {
+//         console.log("e.target ENTER => ", e.target.closest("article"));
+//         e.target.closest("article").classList.remove("drag-start");
+//         // e.target.closest("article").classList.add("drag-enter");
+//     };
 
-    article.ondragleave = (e) => {
-        console.log("e.target LEAVE => ", e.target.closest("article"));
-        // e.target.closest("article").classList.remove("drag-enter");
-    };
+//     article.ondragleave = (e) => {
+//         console.log("e.target LEAVE => ", e.target.closest("article"));
+//         // e.target.closest("article").classList.remove("drag-enter");
+//     };
 
-    article.ondrop = (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        e.target.closest("article").classList.add("drop");
-        // e.target.closest("article").innerHTML = e.dataTransfer.getData("text/plain");
-    };
-    article.ondragend = (e) => {
-        e.target.closest("article").classList.remove("drop");
-    };
-});
+//     article.ondrop = (e) => {
+//         e.stopPropagation();
+//         e.preventDefault();
+//         e.target.closest("article").classList.add("drop");
+//         // e.target.closest("article").innerHTML = e.dataTransfer.getData("text/plain");
+//     };
+//     article.ondragend = (e) => {
+//         e.target.closest("article").classList.remove("drop");
+//     };
+// });
